ba933eaf7cd3e4c44ca6a3e913ae5283a0bfa90f
PG300604 <priyanshughosh97@gmail.com>
2025-04-08 12:26:51 +0530

    Initial commit

---
+++ comp/examples/codes/encapsulation/Encapsulation.java
@@ -0,0 +1,31 @@
+package comp.examples.codes.encapsulation;
+
+public class Encapsulation {
+    private String name;
+    private int Roll;
+    private String college;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return Roll;
+    }
+
+    public void setRoll(int roll) {
+        Roll = roll;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+}
