93a63cf371567dd962d58c5929851a39b9685386
PG300604 <priyanshughosh97@gmail.com>
2025-04-08 12:26:51 +0530

    Initial commit

---
+++ comp/examples/codes/threads/Design.java
@@ -0,0 +1,11 @@
+package comp.examples.codes.threads;
+
+import javax.swing.*;
+
+public class Design {
+    private JPanel panel1;
+    private JTextPane textPane1;
+    private JButton registerButton;
+    private JScrollBar scrollBar1;
+    private JTextField textField1;
+}
